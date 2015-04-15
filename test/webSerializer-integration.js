64ee8415b82bf2efd4ff2271aa30a1a6950c73f7
Dino Scheidt <github@dino-scheidt.de>
2015-04-15 18:51:35 +0300

    Add integration test for .fetchValues for UoC WiSo example

--- test/webSerializer-integration.js
+++ test/webSerializer-integration.js
@@ -1,26 +1,24 @@
 var showIncompleteTest = true;
 
 /*
-* Integration Testing
+* Integration Testing UoC
 * */
-Tinytest.add('webSerializer - integration: unstructured tests - UoC, WiSo Exam Registration Page - Fetch some elements such as the table title by CSS Selector and do some transformations.', function (test) {
+Tinytest.add('webSerializer - integration: unstructured tests - UoC, WiSo Exam Registration Page - Fetch some elements from the Page, such as the table title by CSS Selector, and do some transformations.', function (test) {
 
 	// retrieve a whole bunch of elements from a website
-	var elements = webSerializer({url: fixture.url.valid}).fetchValues([
+	var result = webSerializer({url: fixture.url.valid}).fetchValues([
 		{
 			key: "subTitle",
 			cssSelector: "#box > h2",
-			 // optional (key will be the array index)
-			label: "Page Sub-Title of the List" // optional
+			label: "Page Sub-Title of the List"
 		},
 		{
 			cssSelector: "#box > h1",
-			key: "pageTitle", // optional (key will be the array index)
-			label: "Page Title of the List", // optional
-			// if not specified, selected value is returned as text
-			transform: function (value) { // optional
+			key: "pageTitle",
+			label: "Page Title of the List",
+			transform: function (value) {
 				// manipulate value
-				value = "Trottel " + value.text;
+				value = "Test-Prefix: " + value.text;
 				return value;
 			}
 		},
@@ -33,7 +31,12 @@ Tinytest.add('webSerializer - integration: unstructured tests - UoC, WiSo Exam R
 		}
 
 	]);
-	console.log(elements);
+	//console.log(result);
 
-	test.isFalse(showIncompleteTest, "TODO: This test needs to be written!");
+	test.equal(result.subTitle.value, "Übersicht aktueller Prüfungstermine", "Fetches h2");
+	test.equal(result.pageTitle.value, "Test-Prefix: Prüfungsverwaltung des WiSo-Prüfungsamtes", "Transform Function adds Test-Prefix: ");
+	test.equal(result.pageTitle.label, "Page Title of the List", "Specified label is included");
+	test.equal(result.pageTitle.cssSelector, "#box > h1", "Specified cssSelector is included");
+	test.matches(result[3].value, /Universität zu Köln/i, "If no key specified, provides value under kay as array position.");
+	test.include(result, "subTitle_4", "Not possible to overwrite values with the same key. Instead names key keyname_arrayIndex");
 });
